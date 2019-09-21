d9a0089bff6e1150dbf4d8ec6736d957ad7a9f52
Daniel Adu-Djan <adudjan.danieladd@gmail.com>
2019-09-21 14:27:32 -0400

    added test cases for associative array

--- test.php
+++ test.php
@@ -2,4 +2,8 @@
 	$name_array = array(array("Daniel", array("Adu", array("-"), "Djan")), "Hi", "How", "are", "you", "doing?");
 	
 	php_dump($name_array, "Name");
+	echo "<br>";
+	php_dump($_SERVER, "Server");
+	echo "<br>";
+	php_dump($_REQUEST, "Request");
 ?>
